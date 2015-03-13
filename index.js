1adea7529accbb2d95766a2487614bdeb1c95b54
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-03-13 10:14:53 +0100

    be newrelic object accesible in sails

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = function(sails) {
   return {
     initialize: function(next) {
+      sails.newrelic = newrelic
       sails.on('router:route', function(route) {
         if (route.req.options && route.req.options.controller) {
           return sails.newrelic.setControllerName(route.req.options.controller, route.req.options.action);
--- register.js
+++ register.js
@@ -4,7 +4,7 @@ var path = require('path');
 var config = require(path.resolve('config/newrelic')).newrelic;
 
 (function() {
-  require('newrelic-config')
+  global.newrelic = require('newrelic-config')
   .name(config.app_name)
   .key(config.license_key)
   .log(null, config.logging.level, config.logging.rules.ignore.join(','))
